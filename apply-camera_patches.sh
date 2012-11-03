3af056470643a5f42122cf28a50c8ea3bd5d5bae
Tomasz Rostanski <rozteck@interia.pl>
2012-11-03 09:07:12 +0100

    Add 3:1 precompiled audio, nfs and wifi kernel modules
    No need to rebuild the wifi stuff

--- apply-camera_patches.sh
+++ apply-camera_patches.sh
@@ -7,13 +7,13 @@ pushd frameworks/base
 
 #camera parameters
 echo "16102 BEGIN"
-git fetch http://review.cyanogenmod.com/CyanogenMod/android_frameworks_base refs/changes/02/16102/1 && git cherry-pick FETCH_HEAD
+git pull http://r.cyanogenmod.com/CyanogenMod/android_frameworks_base refs/changes/02/16102/1
 echo "16102 END"
 
 
-#htc camera overlay http://review.cyanogenmod.com/#/c/13285/
+#htc camera overlay http://r.cyanogenmod.com/#/c/13285/
 echo "13285 BEGIN"
-git fetch http://review.cyanogenmod.com/CyanogenMod/android_frameworks_base refs/changes/85/13285/8 && git cherry-pick FETCH_HEAD
+git pull http://r.cyanogenmod.com/CyanogenMod/android_frameworks_base refs/changes/85/13285/8
 echo "13285 END"
 
 popd
@@ -23,33 +23,48 @@ pushd device/hp/tenderloin
 
 #camera permissions
 echo "16120 BEGIN"
-git fetch http://review.cyanogenmod.com/CyanogenMod/android_device_hp_tenderloin refs/changes/20/16120/1 && git cherry-pick FETCH_HEAD
+git pull http://r.cyanogenmod.com/CyanogenMod/android_device_hp_tenderloin refs/changes/20/16120/1
 echo "16120 END"
 
 #memory split
 echo "16121 BEGIN"
-git fetch http://review.cyanogenmod.com/CyanogenMod/android_device_hp_tenderloin refs/changes/21/16121/1 && git cherry-pick FETCH_HEAD
+git pull http://r.cyanogenmod.com/CyanogenMod/android_device_hp_tenderloin refs/changes/21/16121/1
 echo "16121 END"
 
 #compat binder
 echo "16122 BEGIN"
-git fetch http://review.cyanogenmod.com/CyanogenMod/android_device_hp_tenderloin refs/changes/22/16122/1 && git cherry-pick FETCH_HEAD
+git pull http://r.cyanogenmod.com/CyanogenMod/android_device_hp_tenderloin refs/changes/22/16122/1
 echo "16122 END"
 
 #camera.apk config overlay
 echo "19250 BEGIN"
-git fetch http://review.cyanogenmod.com/CyanogenMod/android_device_hp_tenderloin refs/changes/50/19250/3 && git cherry-pick FETCH_HEAD
+git pull http://r.cyanogenmod.com/CyanogenMod/android_device_hp_tenderloin refs/changes/50/19250/3
 echo "19250 END"
 
 #media_profiles.xml
 echo "25296 BEGIN"
-git fetch http://review.cyanogenmod.com/CyanogenMod/android_device_hp_tenderloin refs/changes/96/25296/1 && git cherry-pick FETCH_HEAD
+git pull http://r.cyanogenmod.com/CyanogenMod/android_device_hp_tenderloin refs/changes/96/25296/1
 echo "25296 END"
 
 #video encoder
 echo "25298 BEGIN"
-git fetch http://review.cyanogenmod.com/CyanogenMod/android_device_hp_tenderloin refs/changes/98/25298/1 && git cherry-pick FETCH_HEAD
+git pull http://r.cyanogenmod.com/CyanogenMod/android_device_hp_tenderloin refs/changes/98/25298/1
 echo "25298 END"
+
+#3:1 split audio
+echo "24711 BEGIN"
+git pull http://r.cyanogenmod.com/CyanogenMod/android_device_hp_tenderloin refs/changes/11/24711/1
+echo "24711 END"
+
+#3:1 split nfs modules
+echo "24725 BEGIN"
+git pull http://r.cyanogenmod.com/CyanogenMod/android_device_hp_tenderloin refs/changes/25/24725/2
+echo "24725 END"
+
+#3:1 split wifi modules
+echo "24724 BEGIN"
+git pull http://r.cyanogenmod.com/CyanogenMod/android_device_hp_tenderloin refs/changes/24/24724/2
+echo "24724 END"
 
 popd
 
@@ -58,17 +73,17 @@ pushd packages/apps/Camera
 
 #camera controls
 echo "25233 BEGIN"
-git fetch http://review.cyanogenmod.com/CyanogenMod/android_packages_apps_Camera refs/changes/33/25233/1 && git cherry-pick FETCH_HEAD
+git pull http://r.cyanogenmod.com/CyanogenMod/android_packages_apps_Camera refs/changes/33/25233/1
 echo "25233 END"
 
 #restart cam on mode change
 echo "25234 BEGIN"
-git fetch http://review.cyanogenmod.com/CyanogenMod/android_packages_apps_Camera refs/changes/34/25234/1 && git cherry-pick FETCH_HEAD
+git pull http://r.cyanogenmod.com/CyanogenMod/android_packages_apps_Camera refs/changes/34/25234/1
 echo "25234 END"
 
 #disable panorama
 echo "25295 BEGIN"
-git fetch http://review.cyanogenmod.com/CyanogenMod/android_packages_apps_Camera refs/changes/95/25295/1 && git cherry-pick FETCH_HEAD
+git pull http://r.cyanogenmod.com/CyanogenMod/android_packages_apps_Camera refs/changes/95/25295/1
 echo "25295 END"
 
 popd
