10c2d0227f105acc7a6cc489c2eaef03dad1b55b
Tomasz Rostanski <rozteck@interia.pl>
2012-11-03 09:06:50 +0100

    Add additional patches script

---
+++ apply-additional-patches.sh
@@ -0,0 +1,14 @@
+
+pushd device/hp/tenderloin
+
+#touch pressure
+echo "20894 BEGIN"
+git pull http://r.cyanogenmod.com/CyanogenMod/android_device_hp_tenderloin refs/changes/94/20894/1
+echo "20894 END"
+
+#magnetometer
+echo "24215 BEGIN"
+git pull http://r.cyanogenmod.com/CyanogenMod/android_device_hp_tenderloin refs/changes/15/24215/1
+echo "24215 END"
+
+popd
