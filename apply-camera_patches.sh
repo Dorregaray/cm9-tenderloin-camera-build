684685e3ef6f88a344cfc7bdad7d5680b08196de
Tomasz Rostanski <rostan@podkowa.net>
2012-12-04 16:24:50 +0100

    Update patches

--- apply-camera_patches.sh
+++ apply-camera_patches.sh
@@ -12,29 +12,6 @@ echo "25957 BEGIN"
 git pull http://r.cyanogenmod.org/CyanogenMod/android_frameworks_base refs/changes/57/25957/2
 echo "25957 END"
 
-echo "Patching device"
-cd $TOPDIR/device/hp/tenderloin
-
-# camera permissions
-echo "16120 BEGIN"
-git pull http://r.cyanogenmod.com/CyanogenMod/android_device_hp_tenderloin refs/changes/20/16120/1
-echo "16120 END"
-
-# camera hal
-echo "16532 BEGIN"
-git pull http://r.cyanogenmod.com/CyanogenMod/android_device_hp_tenderloin refs/changes/32/16532/10
-echo "16532 END"
-
-# media_profiles.xml
-echo "25296 BEGIN"
-git pull http://r.cyanogenmod.com/CyanogenMod/android_device_hp_tenderloin refs/changes/96/25296/2
-echo "25296 END"
-
-# video encoder
-echo "25298 BEGIN"
-git pull http://r.cyanogenmod.com/CyanogenMod/android_device_hp_tenderloin refs/changes/98/25298/1
-echo "25298 END"
-
 echo "Patching Camera.apk"
 cd $TOPDIR/packages/apps/Camera
 
