891f1fa8d4e50ecaf39ee871bb9e1f8b21a596bd
Tomasz Rostanski <rostan@podkowa.net>
2012-12-17 15:41:01 +0100

    All camera patches are included in CM9

--- apply-additional-patches.sh
+++ apply-additional-patches.sh
@@ -9,9 +9,4 @@ echo "20894 BEGIN"
 git pull http://r.cyanogenmod.com/CyanogenMod/android_device_hp_tenderloin refs/changes/94/20894/1
 echo "20894 END"
 
-#magnetometer
-echo "24215 BEGIN"
-git pull http://r.cyanogenmod.com/CyanogenMod/android_device_hp_tenderloin refs/changes/15/24215/1
-echo "24215 END"
-
 cd $TOPDIR
--- apply-camera_patches.sh
+++ apply-camera_patches.sh
@@ -2,24 +2,4 @@
 
 TOPDIR=`pwd`
 
-echo "Applying camera patches"
-
-echo "Patching frameworks"
-cd $TOPDIR/frameworks/base
-
-# preview rotation
-echo "25957 BEGIN"
-git pull http://r.cyanogenmod.org/CyanogenMod/android_frameworks_base refs/changes/57/25957/2
-echo "25957 END"
-
-echo "Patching Camera.apk"
-cd $TOPDIR/packages/apps/Camera
-
-# camera controls
-echo "25233 BEGIN"
-git pull http://r.cyanogenmod.com/CyanogenMod/android_packages_apps_Camera refs/changes/33/25233/3
-echo "25233 END"
-
-cd $TOPDIR
-
-echo "Patching done"
+echo "No patches required"
